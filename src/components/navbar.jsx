4d28b5d35cbac6e367e0609eeb433c8c01b1476f
Abdifatah Mohamed <abdifatahmdev@gmail.com>
2020-08-15 00:02:57 -0500

    Added comment on navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-// Stateless Functional Component
-
+// Implementing class based component
 const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
